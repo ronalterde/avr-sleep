d0137b76f672869874abdf737fce6c7cf661307f
Steffen Ronalter <ronalterde@gmail.com>
2017-12-03 17:29:00 +0100

    Add part2 extensions

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Sample code for the blog article *AVR sleep mode for fun and profit*
-[Link to blog](http://steffen.ronalter.de/2017/11/26/avr-sleep-mode-for-fun-and-profit/)
+[Link to blog part 1](http://steffen.ronalter.de/2017/11/26/avr-sleep-mode-for-fun-and-profit/)
+[Link to blog part 2](http://steffen.ronalter.de/2017/12/03/avr-sleep-mode-for-fun-and-profit-part-2/)
 
 To build the project (AVR libc is a prerequisite):
 ```
--- main.c
+++ main.c
@@ -1,4 +1,26 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define F_CPU 16000000UL
+#include <util/delay.h>
+
+ISR(INT2_vect) {
+}
+
+static void configurePortPins() {
+	DDRB |= (1<<PB6); // Output LED
+	DDRB &= ~(1<<PB2); // INT2 input
+	PORTB |= (1<<PB2); // pull-up for INT2
+}
+
+static void configureInterrupts() {
+	// Enable interrupt on falling edge of INT2
+	MCUCSR &= ~(1<<ISC2);
+	GICR |= (1<<INT2);
+	
+	// Enable interrupts globally
+	sei();
+}
 
 static void setSleepModePowerDown() {
 	MCUCR &= ~(1<<SM2);
@@ -15,14 +37,21 @@ static void disableSleepMode() {
 	MCUCR &= ~(1<<SE);
 }
 
+static void toggleLED() {
+	PORTB |= (1<<PB6);
+	_delay_ms(2000);
+	PORTB &= ~(1<<PB6);
+}
+
 int main(void) {
+	configurePortPins();
+	configureInterrupts();
+
 	while(1) {
 		setSleepModePowerDown();
 		enterSleepMode();
-
-		// [things to do after wake-up]
-
 		disableSleepMode();
+		toggleLED();
 	}
 
 	return 0;
